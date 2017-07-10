a06c08adb6449219b824e01af97eb8f50b91b5a9
Borja Garcia Villanueva <borjagarciavillanueva@gmail.com>
2017-07-10 23:40:14 +0200

    Models for cpu, motherboards and graphic cards created

---
+++ models/cpu.js
@@ -0,0 +1,10 @@
+var cpu = [
+    { name: 'cpu1', price: 150, src:'./images/cpu/cpu1.jpg' },
+    { name: 'cpu2', price: 250, src:'./images/cpu/cpu2.jpg' },
+    { name: 'cpu3', price: 350, src:'./images/cpu/cpu3.jpg' },
+    { name: 'cpu4', price: 450, src:'./images/cpu/cpu4.jpg' },
+    { name: 'cpu5', price: 550, src:'./images/cpu/cpu5.jpg' },
+    { name: 'cpu6', price: 650, src:'./images/cpu/cpu6.jpg' },
+    { name: 'cpu7', price: 750, src:'./images/cpu/cpu7.jpg' },
+    { name: 'cpu8', price: 850, src:'./images/cpu/cpu8.jpg' }
+];
---
+++ models/motherboards.js
@@ -0,0 +1,10 @@
+var motherboards = [
+    { name: 'motherboard1', price: 175, src: './images/motherboards/motherboard1.jpg' },
+    { name: 'motherboard2', price: 275, src: './images/motherboards/motherboard2.jpg' },
+    { name: 'motherboard3', price: 375, src: './images/motherboards/motherboard3.jpg' },
+    { name: 'motherboard4', price: 475, src: './images/motherboards/motherboard4.jpg' },
+    { name: 'motherboard5', price: 575, src: './images/motherboards/motherboard5.jpg' },
+    { name: 'motherboard6', price: 675, src: './images/motherboards/motherboard6.jpg' },
+    { name: 'motherboard7', price: 775, src: './images/motherboards/motherboard7.jpg' },
+    { name: 'motherboard8', price: 875, src: './images/motherboards/motherboard8.jpg' }
+];
